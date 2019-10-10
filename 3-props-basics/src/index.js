424d5175e5bd65633978cca4d6d672c33c596f27
ns <serhii.nesteruk@gmail.com>
2019-10-10 10:10:26 +0300

    finished part 3

--- 3-props-basics/src/index.js
+++ 3-props-basics/src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ComponentDetail from './ComponentDetail'
+import ApprovalCard from './ApprovalCard'
+
+const App = () => {
+    return (
+        <div className="ui container comments">
+            <ApprovalCard>
+                <ComponentDetail author="Lol" text="hoy there" />
+            </ApprovalCard>
+            <ComponentDetail author="Lel" text="hey there" />
+            <ComponentDetail author="Lil" text="hi there" />
+        </div>
+    );
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
